2984744cb378e4e410438d4d2b07854f996eac02
geer26 <gergo.kurucz@gmail.com>
2021-02-14 10:59:41 +0100

    OPTv0.1

---
+++ app/__init__.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from config import SQLite
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from flask_socketio import SocketIO
+
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config.from_object(SQLite)
+
+login = LoginManager(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+with app.app_context():
+    if db.engine.url.drivername == 'sqlite':
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
+
+
+socket = SocketIO(app)
+socket.init_app(app, cors_allowed_origins="*")
+
+
+# define event class
+class Event():
+    code = 0
+    name = ''
+    response = ''
+
+    def __init__(self, code, response, name):
+        self.name = name
+        self.code = code
+        self.response = response
+
+from app import routes, models
---
+++ config.py
@@ -0,0 +1,46 @@
+import os
+from os import environ, path
+from dotenv import load_dotenv
+
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
+class Config:
+    """Base config."""
+    SECRET_KEY = environ.get('SECRET_KEY') or '01!ChAnGeThIs!89'
+    SESSION_COOKIE_NAME = environ.get('SESSION_COOKIE_NAME') or '!Opt2021!'
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+
+    TEMPLATES_AUTO_RELOAD = True
+    FLASK_ENV = 'development'
+    DEBUG = True
+    TESTING = True
+
+    UPLOAD_FOLDER = path.join('app/uploads/')
+    #CLIENT_LISTS = path.join('app/temp/')
+    ALLOWED_EXTENSIONS = {'zip'}
+    #SENDGRID_API_KEY = environ.get('SENDGRID_API_KEY') or 'APIKEY'
+    #LANG = environ.get('LANG') or 'C.UTF-8'
+    #LC_ALL = environ.get('LC_ALL') or 'C.UTF-8'
+
+class PostgreSQL(Config):
+    """Production config."""
+    #FLASK_ENV = 'production'
+    #DEBUG = False
+    #TESTING = False
+    #SQLALCHEMY_TRACK_MODIFICATIONS = False
+    #SQLALCHEMY_DATABASE_URI = environ.get('POSTGRES_URI')
+
+
+class SQLite(Config):
+    """Development config."""
+    FLASK_ENV = 'development'
+    DEBUG = True
+    TESTING = True
+    DATABASE_URI = environ.get('DEV_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLITE_URI') or \
+                              'sqlite:///' + path.join(basedir, 'data.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
