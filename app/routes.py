2ade0f7ae2fbb9003be5dc4b0815c1a3c9cda075
geer26 <gergo.kurucz@gmail.com>
2021-02-15 10:03:47 +0100

    OPTv0.1

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ import json
 import os
 
 from flask import render_template
+from flask_login import current_user, login_user, logout_user, login_required
 
 from app import app, socket, db
 
@@ -9,4 +10,5 @@ from app import app, socket, db
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
-    return render_template('base.html')
+    if not current_user.is_authenticated:
+        return render_template('/noauth/index.html')
