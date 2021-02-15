e87907660711898d2585ca011537eb4d6c34dde3
geer26 <gergo.kurucz@gmail.com>
2021-02-15 15:32:43 +0100

    OPTv0.1

--- app/routes.py
+++ app/routes.py
@@ -10,5 +10,17 @@ from app import app, socket, db
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
+    #if user is not autheticated, display noauth index.html
     if not current_user.is_authenticated:
         return render_template('/noauth/index.html')
+    #else if user is superuser display su index.html
+    elif current_user.is_authenticated and current_user.is_superuser:
+        return '',204
+    # else if user is not superuser display user index.html
+    elif current_user.is_authenticated and not current_user.is_superuser:
+        return '', 204
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    return render_template('/noauth/login.html', title = 'Belépés')
