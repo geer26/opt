f338c2bb5bcf97b8b55cd9c8e4ddc6dcf084ac3a
geer26 <gergo.kurucz@gmail.com>
2021-02-15 09:38:00 +0100

    OPTv0.1

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 import json
 import os
 
+from flask import render_template
+
 from app import app, socket, db
 
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
 def index():
-    return('HELLO WORLD!')
+    return render_template('base.html')
