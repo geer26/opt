e87ee8e29c7fd368bf986305ed52d34cfcb45275
geer26 <gergo.kurucz@gmail.com>
2021-02-18 09:43:18 +0100

    OPTv0.1

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 import json
 import os
 
-from flask import render_template, redirect
+from flask import render_template, redirect, request
 from flask_login import current_user, login_user, logout_user, login_required
-
+from app.forms import LoginForm
 from app import app, socket, db
 from app.workers import hassu, generate_rnd
 from app.models import User
@@ -15,17 +15,31 @@ def index():
     #if user is not autheticated, display noauth index.html
     if not current_user.is_authenticated:
         return render_template('/noauth/index.html')
-    #else if user is superuser display su index.html
+    #else if user is superuser display admin index.html
     elif current_user.is_authenticated and current_user.is_superuser:
-        return '',204
+        return render_template('/admin/index.html')
     # else if user is not superuser display user index.html
     elif current_user.is_authenticated and not current_user.is_superuser:
-        return '', 204
+        return render_template('/user/index.html')
 
 
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('/noauth/login.html', title = 'Belépés')
+
+    if current_user.is_authenticated:
+        return redirect('/')
+
+    form = LoginForm()
+
+    if request.method == 'POST' and not current_user.is_authenticated:
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.data).first()
+            if user.check_password(form.password.data):
+                login_user(user, remember=form.remember_me.data)
+            return redirect('/')
+        else:
+            return redirect('/') #SEND ERROR MESSAGE?
+    return render_template('/noauth/login.html', title = 'Belépés', form = form)
 
 
 @login_required
