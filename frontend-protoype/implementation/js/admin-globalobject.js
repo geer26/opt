7fcc240b04497df5d2928944703e112d953647ce
LeoMinor <kurucz.gyozo@gmail.com>
2021-02-23 10:26:10 +0100

    minor fixes

---
+++ frontend-protoype/implementation/js/admin-globalobject.js
@@ -0,0 +1,54 @@
+var admin_data = {
+  "current_user": {
+    "id": 2,
+    "username": "LeoMinor",
+    "description": "Adminisztrátor",
+    "contact": "wergrtg",
+    "is_superuser": true,
+    "settings": "",
+    "added": "2021.02.23",
+    "last_modified": "2021.02.23, 09:00:56"
+  },
+  "users": [
+    {
+      "id": 1,
+      "username": "geer26",
+      "description": "Adminisztrátor felhasználó",
+      "contact": "none@none.no",
+      "is_superuser": true,
+      "settings": "",
+      "added": "2021.02.21",
+      "last_modified": "2021.02.21, 06:49:49"
+    },
+    {
+      "id": 2,
+      "username": "LeoMinor",
+      "description": "Adminisztrátor",
+      "contact": "wergrtg",
+      "is_superuser": true,
+      "settings": "",
+      "added": "2021.02.23",
+      "last_modified": "2021.02.23, 09:00:56"
+    },
+       {
+      "id": 3,
+      "username": "Pél Dani",
+      "description": "Felhasználó",
+      "contact": "wergrtg",
+      "is_superuser": false,
+      "settings": "",
+      "added": "2021.02.23",
+      "last_modified": "2021.02.23, 09:00:56"
+    },
+    {
+      "id": 4,
+      "username": "Pró Bálint",
+      "description": "Adminisztrátor",
+      "contact": "wergrtg",
+      "is_superuser": false,
+      "settings": "",
+      "added": "2021.02.23",
+      "last_modified": "2021.02.23, 09:00:56"
+    }
+  ]
+};
