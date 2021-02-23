868a13a81c91480a7bbbfd19d7ed028a988e2e12
LeoMinor <kurucz.gyozo@gmail.com>
2021-02-23 18:31:06 +0100

    sorbarendezés tetszőleges szempont alapján hozzáadva

--- frontend-protoype/implementation/components/table-component/TableComponent.js
+++ frontend-protoype/implementation/components/table-component/TableComponent.js
@@ -7,7 +7,16 @@ const TableComponent = {
 		<table>
 			<thead>
 				<tr>
-					<th v-for="field in fields" v-bind:style="{width: field.stretch ? '100%' : ''}">{@ field.title @}</th>
+					<th v-for="field in fields" v-bind:style="{width: field.stretch ? '100%' : ''}">
+						{@ field.title @}
+						<span class="sorter" 
+							  v-if="field.sortable" 
+							  v-on:click="applySort(field.name)">
+							<i v-if="field.name == sort.by && sort.direction == 0" class="fa fa-sort-down"></i>
+							<i v-else-if="field.name == sort.by && sort.direction == 1" class="fa fa-sort-up"></i>
+							<i v-else class="fa fa-sort"></i>
+						</span>
+					</th>
 				</tr>
 			</thead>
 			<tbody>
@@ -33,7 +42,11 @@ const TableComponent = {
 			currentPage: 0,
 			currentCount: 25,
 
-			selected: null
+			selected: null,
+			sort: {
+				by: null,
+				direction: 0,
+			}
 		}
 	},
 
@@ -97,6 +110,24 @@ const TableComponent = {
 
 		resetSelection() {
 			this.selected = null;
+		},
+
+		applySort(name) {
+			if (this.sort.by == null) {
+				this.sort.by = name;
+				this.sort.direction = 0;
+			} else if (this.sort.by == name) {
+				// Ha ugyanarra klikkelünk rá, akkor forgatjuk az irányt
+				this.sort.direction = (this.sort.direction + 1) % 3;
+				// Ha az irány a 2, akkor nem rendezünk sorba
+				if (this.sort.direction == 2) {
+					this.sort.by = null;
+					this.sort.direction = 0;
+				}
+			} else {
+				this.sort.by = name;
+				this.sort.direction = 0;
+			}
 		}
 	},
 
@@ -104,8 +135,19 @@ const TableComponent = {
 
 	computed: {
 		selectedRecords() {
+			// SORBARENDEZÉS
+			var recs = this.records;
+			if (this.sort.by != null) {
+				// Itt először sorba kell rendeznünk, ha egyáltalán van megadva erre szabály
+				if (this.sort.direction == 0)				
+					recs.sort((a,b) => (a[this.sort.by] > b[this.sort.by]) ? 1 : -1)
+				if (this.sort.direction == 1)
+					recs.sort((a,b) => (a[this.sort.by] < b[this.sort.by]) ? 1 : -1)
+			}
+
+			// KIVÁLASZTOTT REKORDOK
 			var start = this.currentCount * this.currentPage;
-			return this.records.slice(start, start + this.currentCount);
+			return recs.slice(start, start + this.currentCount);
 		},
 
 		numPages() {
